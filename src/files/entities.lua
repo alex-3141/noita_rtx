23a2290fa5054c69be1b6d4beea9dde0aeb9f4d9
Alex <aanderson0@protonmail.com>
2026-07-05 21:01:36 +0900

    Fix portal lightcomponent offsets

--- src/files/entities.lua
+++ src/files/entities.lua
@@ -18,10 +18,52 @@ local function patch_effect_hit_ground()
     end
 end
 
+local function patch_teleporter_lightcomponent_offsets()
+    for _, path in ipairs({
+        "data/entities/buildings/mystery_teleport.xml",
+        "data/entities/buildings/mystery_teleport_back.xml",
+        "data/entities/buildings/teleport_bunker.xml",
+        "data/entities/buildings/teleport_bunker2.xml",
+        "data/entities/buildings/teleport_bunker_back.xml",
+        "data/entities/buildings/teleport_desert.xml",
+        "data/entities/buildings/teleport_end_wall.xml",
+        "data/entities/buildings/teleport_ending_victory_delay.xml",
+        "data/entities/buildings/teleport_ending_victory.xml",
+        "data/entities/buildings/teleport_ending.xml",
+        "data/entities/buildings/teleport_excavationsite_cube_return.xml",
+        "data/entities/buildings/teleport_hourglass_return.xml",
+        "data/entities/buildings/teleport_hourglass.xml",
+        "data/entities/buildings/teleport_liquid_powered.xml",
+        "data/entities/buildings/teleport_meditation_cube_return.xml",
+        "data/entities/buildings/teleport_meditation_cube.xml",
+        "data/entities/buildings/teleport_robot_egg_return.xml",
+        "data/entities/buildings/teleport_snowcave_buried_eye.xml",
+        "data/entities/buildings/teleport_start.xml",
+        "data/entities/buildings/teleport_teleroom_1.xml",
+        "data/entities/buildings/teleport_teleroom_2.xml",
+        "data/entities/buildings/teleport_teleroom_3.xml",
+        "data/entities/buildings/teleport_teleroom_4.xml",
+        "data/entities/buildings/teleport_teleroom_5.xml",
+        "data/entities/buildings/teleport_teleroom_6.xml",
+        "data/entities/buildings/teleport_teleroom.xml",
+        "data/entities/misc/greed_curse/greed_ghost_portal.xml",
+        "data/entities/projectiles/deck/summon_portal_teleport.xml",
+        "data/entities/projectiles/deck/tentacle_portal.xml",
+    }) do
+        local xml = nxml.parse(ModTextFileGetContent(path))
+
+        for comp in xml:each_of("LightComponent") do
+            comp.attr.offset_x = "0"
+            comp.attr.offset_y = "0"
+        end
+
+        ModTextFileSetContent(path, tostring(xml))
+    end
+end
+
 local function patch()
     patch_effect_hit_ground()
-
-    print("Entities Patched")
+    patch_teleporter_lightcomponent_offsets()
 end
 
 
